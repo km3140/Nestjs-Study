11048e72fdf2fe3b7a562a0c875696ee137711e9
km3140 <kimn3140@naver.com>
2023-05-08 22:19:21 +0900

    bcrypt

--- 02_moveiAPI-typeORM/src/auth/user.repository.ts
+++ 02_moveiAPI-typeORM/src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -11,7 +16,19 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
-    await this.save(user);
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505')
+        //                👆 unique 데코레이터에 걸렸을 때 에러코드
+        throw new ConflictException('Existing username');
+      else throw new InternalServerErrorException();
+    }
   }
 }
