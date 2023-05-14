dcc6a8b3d239b4f904717490cb80c67e6d3a921a
km3140 <kimn3140@naver.com>
2023-05-14 21:46:21 +0900

    chore

--- 02_moveiAPI-typeORM/src/app.module.ts
+++ 02_moveiAPI-typeORM/src/app.module.ts
@@ -7,12 +7,12 @@ import { AuthModule } from './auth/auth.module';
 
 // 루트 모듈
 @Module({
-  // 👇 url경로별로 라우터를 분리시킨 것과 비슷
   imports: [TypeOrmModule.forRoot(typeORMConfig), MoviesModule, AuthModule],
   //                      👆 dynamic-module
+  // 👇 url경로별로 라우터를 분리시킨 것과 비슷
   controllers: [AppController], // 👈 이 곳에는 AppController만 와야함!
   //            👆 nest g(enerate) co(ntroller) -> movies.controllers.ts 생성
   providers: [], // 👈 이 곳에는 AppService만 와야함!
-  //            👆 nest g(enerate) s(ervice) -> movies.service.ts 생성
+  //         👆 nest g(enerate) s(ervice) -> movies.service.ts 생성
 })
 export class AppModule {}
